c9b4e3fe415edb18579aaf8ea362a1dd845153e6
robin <robin.j.soto@gmail.com>
2020-11-11 14:14:56 -0800

    hard codes one map marker and popup

--- src/views/ConnectionsMap.vue
+++ src/views/ConnectionsMap.vue
@@ -18,6 +18,13 @@ export default {
   data: function() {
     return {
       message: "Your Travel Universe",
+      friendCities: [
+        {
+          long: -122.4194,
+          lat: 37.7749,
+          description: "San Francisco",
+        },
+      ],
     };
   },
   mounted: function() {
@@ -25,8 +32,17 @@ export default {
     const map = new mapboxgl.Map({
       container: "map",
       style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
-      center: [-122.431297, 37.773972], // starting position [lng, lat]
-      zoom: 10, // starting zoom
+      center: [-16.6291, 28.2916], // starting position [lng, lat]
+      zoom: 1.5, // starting zoom
+    });
+    this.friendCities.forEach((friendCity) => {
+      var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+        friendCity.description
+      );
+      var marker = new mapboxgl.Marker()
+        .setLngLat([friendCity.long, friendCity.lat])
+        .setPopup(popup)
+        .addTo(map);
     });
   },
   methods: {},
