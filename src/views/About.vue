836b30e9f72dcc3f1c77fac743f85b0778765b92
robin <robin.j.soto@gmail.com>
2020-11-16 14:12:18 -0800

    finishes about page basics

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,101 @@
 <template>
   <div class="about">
-    <h1>Plan your next adventure with Traverse</h1>
+    <section class="lg bg-white">
+      <div class="container">
+        <div class="section-title-area mb-50">
+          <h3 class="section-title">Plan your next adventure with Traverse</h3>
+          <h5 class="section-sub-title">
+            Visualize your travel network
+          </h5>
+        </div>
+        <!-- / section-title-area -->
+        <div class="row v-center">
+          <div class="col-lg-6 tablet-lg-top-30">
+            <div
+              class="promo-box promo-left pt-0 pb-20 bg-transparent no-transform pl-0"
+            >
+              <div class="promo-container-big">
+                <div
+                  class="promo-big promo-icon-bg border-primary circle primary-shadow-big w-hover"
+                >
+                  <i class="fas fa-people-arrows"></i>
+                </div>
+                <!-- / promo-icon-bg -->
+                <h6
+                  class="box-title mb-15 text-hover info-hover secondary-font"
+                >
+                  Connect to your friends
+                </h6>
+                <p class="box-description">
+                  Find your friends on Traverse to build your own ultimate
+                  resource about every place you'd like to go.
+                </p>
+              </div>
+              <!-- / promo-container -->
+            </div>
+            <!-- / promo-box -->
+
+            <div
+              class="promo-box promo-left pt-0 pb-20 bg-transparent no-transform pl-0"
+            >
+              <div class="promo-container-big">
+                <div
+                  class="promo-big promo-icon-bg border-primary circle primary-shadow-big w-hover"
+                >
+                  <i class="fas fa-map-marked-alt"></i>
+                </div>
+                <!-- / promo-icon-bg -->
+                <h6
+                  class="box-title mb-15 text-hover success-hover secondary-font"
+                >
+                  Build your personalized map
+                </h6>
+                <p class="box-description">
+                  Traverse combines your profile and your friends' profiles to
+                  build a personalized map of your unique travel universe. Just
+                  click on a city to see which of your friends has been there.
+                </p>
+              </div>
+              <!-- / promo-container -->
+            </div>
+            <!-- / promo-box -->
+
+            <div
+              class="promo-box promo-left pt-0 pb-20 bg-transparent no-transform pl-0"
+            >
+              <div class="promo-container-big">
+                <div
+                  class="promo-big promo-icon-bg border-primary circle primary-shadow-big w-hover"
+                >
+                  <i class="fas fa-globe-americas"></i>
+                </div>
+                <!-- / promo-icon-bg -->
+                <h6
+                  class="box-title mb-15 text-hover danger-hover secondary-font"
+                >
+                  Inform your adventures from trusted sources
+                </h6>
+                <p class="box-description">
+                  Explore your map, get inspired, and collect all the advice you
+                  need to plan your next amazing trip.
+                </p>
+              </div>
+              <!-- / promo-container -->
+            </div>
+            <!-- / promo-box -->
+          </div>
+          <!-- / column -->
+
+          <div class="col-lg-6">
+            <img src="assets/images/compass.jpg" alt="" />
+          </div>
+          <!-- / column -->
+        </div>
+        <!-- / row -->
+      </div>
+      <!-- / container -->
+    </section>
+    <!-- <h1>Plan your next adventure with Traverse</h1>
     <br />
     <h3>
       What better way to get the best travel advice than from your own trusted
@@ -18,7 +113,7 @@
       secret beach? Heading to London for the week and want to see who is in
       town for dinner? Eager to provide travel advice of your own?
     </h3>
-    <br />
-    <h2>Set up your profile and start building your Traverse map today!</h2>
+    <br /> -->
+    <!-- <h2>Set up your profile and start building your Traverse map today!</h2> -->
   </div>
 </template>
