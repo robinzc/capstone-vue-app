b06030ba3dbe4f354b501a337fe6858b28cacd50
robin <robin.j.soto@gmail.com>
2020-11-16 11:05:30 -0800

    adds nav and footer, updates app.vue router links and buttons

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,95 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <ul>
-        <span v-if="!isLoggedIn()">
-          <router-link to="/"> Welcome |</router-link>
-        </span>
-        <span>
-          <router-link to="/about"> About |</router-link>
-        </span>
-        <span v-if="!isLoggedIn()">
-          <router-link to="/signup"> Signup |</router-link>
-        </span>
-        <!-- Needs to redirect to UsersNew to populate rest of profile after signing up -->
-        <span v-if="!isLoggedIn()">
-          <router-link to="/login"> Login |</router-link>
-        </span>
-        <span v-if="isLoggedIn()">
-          <router-link to="/logout"> Logout |</router-link>
-        </span>
-        <span v-if="isLoggedIn()">
-          <router-link to="/users"> The Universe |</router-link>
-        </span>
-        <span v-if="isLoggedIn()">
-          <router-link to="/connections"> Connections |</router-link>
-        </span>
-        <span v-if="isLoggedIn()">
-          <router-link v-bind:to="`/users/${getUserId()}`">
-            My Profile |</router-link
-          >
-        </span>
-        <span v-if="isLoggedIn()">
-          <router-link to="/map_connections"> My Map</router-link>
-        </span>
-      </ul>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#x"
+          ><img
+            src="assets/images/logo.svg
+          "
+            alt=""
+        /></a>
+
+        <button
+          class="navbar-toggler collapsed"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbar-toggle1"
+          aria-controls="navbar-toggle1"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="icon-bar top-bar"></span>
+          <span class="icon-bar middle-bar"></span>
+          <span class="icon-bar bottom-bar"></span>
+          <span class="sr-only">Toggle navigation</span></button
+        ><!-- / navbar-toggler -->
+
+        <div class="collapse navbar-collapse" id="navbar-toggle1">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item" v-if="!isLoggedIn()">
+              <router-link class="nav-link active" to="/">
+                Welcome
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/about">
+                About</router-link
+              >
+            </li>
+            <li class="nav-item" v-if="isLoggedIn()">
+              <router-link class="nav-link active" to="/users">
+                The Universe
+              </router-link>
+            </li>
+            <li class="nav-item" v-if="isLoggedIn()">
+              <router-link class="nav-link active" to="/connections">
+                Connections</router-link
+              >
+            </li>
+            <li class="nav-item" v-if="isLoggedIn()">
+              <router-link
+                class="nav-link active"
+                v-bind:to="`/users/${getUserId()}`"
+              >
+                My Profile</router-link
+              >
+            </li>
+            <li class="nav-item" v-if="isLoggedIn()">
+              <router-link class="nav-link active" to="/map_connections">
+                My Map</router-link
+              >
+            </li>
+            <li class="nav-item" v-if="isLoggedIn()">
+              <router-link class="nav-link active" to="/logout">
+                Logout</router-link
+              >
+            </li>
+            <!-- / dropdown -->
+          </ul>
+          <!-- / navbar-nav -->
+
+          <ul class="navbar-button ml-30 m-0 p-0">
+            <li class="nav-item" v-if="!isLoggedIn()">
+              <router-link class="btn btn-primary btn-sm" to="/login">
+                Login</router-link
+              >
+            </li>
+          </ul>
+          <ul class="navbar-button ml-30 m-0 p-0">
+            <li class="nav-item" v-if="!isLoggedIn()">
+              <router-link class="btn btn-primary btn-sm" to="/signup">
+                Signup
+              </router-link>
+            </li>
+          </ul>
+          <!-- / btn-sm -->
+        </div>
+        <!-- / navbar-collapse -->
+      </div>
+      <!-- / container -->
+    </nav>
+    <!-- / navbar -->
+
     <router-view />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,41 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <header
+      class="fh bg-img bg-fixed img-bottom text-center dark"
+      style="background-image: url(/assets/images/main-header.jpg)"
+    >
+      <div class="container">
+        <h1 class="page-title fixed-title mt-50">
+          <span class="d-block">TRAVERSE</span>
+          <span class="d-block fs-24 opc-75"
+            >Inform your travels through your own trusted network</span
+          >
+        </h1>
+      </div>
+      <!-- / container -->
+      <img
+        src="/assets/images/angles.svg"
+        alt=""
+        class="header-image-bottom absolute bottom-0 left-0 right-0"
+      />
+    </header>
+    <a
+      href="#top"
+      class="scroll-to-top is-hidden smooth-scroll"
+      data-nav-status="toggle"
+      ><i class="fas fa-chevron-up"></i
+    ></a>
+    <!-- <h1>{{ message }}</h1>
     <h2>Inform your travels through your own trusted network</h2>
     <br />
-    <h2>Visualize your personal travel universe</h2>
-    <button>
+    <h2>Visualize your personal travel universe</h2> -->
+    <!-- <button>
       <router-link :to="`/signup`"> Sign up</router-link>
     </button>
     <button>
       <router-link :to="`/login`"> Login</router-link>
-    </button>
-    <h3>Signup is free and easy!</h3>
+    </button> -->
+    <!-- <h3>Signup is free and easy!</h3> -->
   </div>
 </template>
 
